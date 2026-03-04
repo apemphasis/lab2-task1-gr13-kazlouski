d1365ec4f89721d7780c1a2503434eea0a11184c
Yauheni Kazlouski <eu.new.gene@gmail.com>
2026-03-04 09:30:17 +0000

    add printcode.h

---
+++ include/printcode.h
@@ -0,0 +1,16 @@
+/* Yauheni Kazlouski */
+
+#ifndef PRINTCODE_H
+#define PRINTCODE_H
+
+/*
+* Функция print_codes выводит коды символов строки str
+* параметр: str строка, которую нужно обработать
+* возвращаемое значение: void
+*/
+
+#include <string.h>
+
+void print_codes(char *str);
+
+#endif
